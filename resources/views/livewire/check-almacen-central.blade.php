8b4f65fa6209d9b613f9efc131c47323fcafcaf8
hacker <mm.helio009@gmail.com>
2024-05-24 19:27:46 -0400

    Se agreo la un campo a la tabla sucursales, para saber cual es el almacen centra, y asi mimo se mostrara en el index un campo de dicha informacion

--- resources/views/livewire/check-almacen-central.blade.php
+++ resources/views/livewire/check-almacen-central.blade.php
@@ -1,7 +1,29 @@
 <div>
-    <div class="mb-3 form-check" style="margin: 5px;">
-        <input type="checkbox" class="form-check-input" id="almacenCentralCheck" wire:model='almcen_central' {{$estado}}>
-        <label class="form-check-label" for="almacenCentralCheck">Sucursal Central (Almacen Central)</label>
+    <div class="mb-3">
+        <label for="nit_sucursal" class="form-label">Nit:</label>
+        <input type="text" class="form-control" name="nit" id="nit_sucursal" aria-describedby="emailHelp" placeholder="Introduzca el Nit" wire:change.live='comprobarAlmacenCentral'> 
     </div>
-    <button wire:click='actulizarCheckAlmacenCentral'> +++</button>
+    <div class="mb-3">
+          <label for="razon_social_sucursal" class="form-label">Razon Social:</label>
+          <input type="text" class="form-control" name="razon_social" id="razon_social_sucursal" aria-describedby="emailHelp" placeholder="Introduzca la Razon Social" wire:change.live='comprobarAlmacenCentral'> 
+    </div>
+    <div class="mb-3">
+          <label for="direccion_sucursal" class="form-label">Direccion:</label>
+          <input type="text" class="form-control" name="direccion" id="direccion_sucursal" aria-describedby="emailHelp" placeholder="Introduzca la Direccion" wire:change.live='comprobarAlmacenCentral'> 
+    </div>
+    <div class="mb-3">
+          <label for="telefonos_sucursal" class="form-label">Telefonos:</label>
+          <input type="text" class="form-control" name="telefonos" id="telefonos_sucursal" aria-describedby="emailHelp" placeholder="Introduzca los Telefonos" wire:change.live='comprobarAlmacenCentral'> 
+    </div>
+    <div class="mb-3">
+        <label for="ciudad_sucursal" class="form-label">Ciudad:</label>
+        <input type="text" class="form-control" name="ciudad" id="ciudad_sucursal" aria-describedby="emailHelp" placeholder="Introduzca la Ciudad" wire:change.live='comprobarAlmacenCentral'> 
+    </div>
+    <div class="mb-3 form-check" style="margin: 5px;">
+        <input type="checkbox" class="form-check-input" name="almacen_central" id="almacenCentralCheck" wire:model='almacen_central' {{$estado}}>
+        <label class="form-check-label" for="almacenCentralCheck">
+            Sucursal Central (Almacen Central) <br>
+            {{$mensaje}}
+        </label>
+    </div>
 </div>
